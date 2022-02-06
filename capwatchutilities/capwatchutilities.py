e2b190de7389c905e03749efd2cbe8d5f37fd1e9
kexela <kexel@dczia.net>
2022-02-06 15:36:49 -0600

    Added basic diagnostic checks

--- capwatchutilities/capwatchutilities.py
+++ capwatchutilities/capwatchutilities.py
@@ -1,21 +1,85 @@
 import logging
 import sys
-from cwuArguments import cwuArguments as cwuA
+import os.path
+from cwuArguments import cwuArguments
+from cwucapnhq    import cwucapnhq
+from cwuWings     import wing
+from cwuO365      import office365
+from cwujson      import cwujson
 
 def main():
     """
     """
     logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+    logging.info( "--------------------------------------------------------------------")
     logging.info(f"Running capwatch utilities . . .")
-    cwuA(sys.argv)
-    logging.info("--------------------------------------------")
-    logging.info("This python provides a basic python library ")
-    logging.info(" to help manage capwatch data for downstram ")
-    logging.info(" use.                                       ")
-    logging.info("--------------------------------------------")
+    cwuA=cwuArguments(sys.argv)
+    logging.info( "--------------------------------------------------------------------")
+    logging.info(" - Loading Wing Organization Information from E-services (Password Required)")
+    zipfile=os.path.join(cwuA.zipdir,cwuA.todays_date+".zip")
+    if(os.path.exists(zipfile) == False):
+        current_cw = cwucapnhq(basedir=cwuA.zipdir,
+                            baseurl=cwuA.baseurl,
+                            capnhqurl=cwuA.capnhqurl,
+                            orgid=cwuA.orgid,
+                            cw_dir=cwuA.basedir,
+                            date=cwuA.todays_date,
+                            output_filename=cwuA.todays_date+".zip")
+        if(current_cw.getcw(username=cwuA.capid,password=cwuA.password) == False):
+            logging.error("Unable to Load CAPWATCH DATABASE, please check password and connection")
+            sys.exit(1)
+    else:
+        logging.info(" Processing Information for WING from: " + zipfile)
+    logging.info( "--------------------------------------------------------------------")
+    logging.info(" - Processing Wing Organization Information into Data Structures")
+    theWing = wing(dateformed=cwuA.todays_date,
+                   capid=cwuA.capid,
+                   wing=cwuA.wingid,
+                   org_id=cwuA.topid,
+                   cap_region=cwuA.regionid,
+                   wing_level_org_id=cwuA.orgid,
+                   basedir=cwuA.basedir)
+    logging.info(" Getting All User Account information")
+    wingJson = cwujson(theWing.json_dir)
+    json_tokens=wingJson.load_json_file(filename="O365.json")
+    logging.info(" - FINISHED Processing Wing Organization Information")
+    logging.info("--------------------------------------------------------------------")
+    logging.info(" - IT Historical JSON")
+    itJSON=wingJson.load_json_file(filename="it.json")
+    ####### FORM {
+    ####### FORM     "384814" : { "Active": true, "MSFT Account Created": true, "PasswordReset": false },
+    ####### FORM     "000000" : { "Active": false, "MSFT Account Created": false, "PasswordReset": false },
+    ####### FORM     "999999" : { "Active": false, "MSFT Account Created": false, "PasswordReset": false }
+    ####### FORM }            
+    logging.info("--------------------------------------------------------------------")
+    logging.info(" Office 365 Administration Startup")
+    office_inst=office365(client_id=json_tokens["client_id"],
+                          client_secret=json_tokens["client_secret"],
+                          tenant_id=json_tokens["tenant_id"],
+                          emailDomain=json_tokens["email_domain"],
+                          admin_client_id=json_tokens["admin_client_id"],
+                          defaultLicense=json_tokens["defaultLicense"]
+                          )
+    if(cwuA.process_groups):
+        logging.info( "--------------------------------------------------------------------")
+        logging.info(" - Processing Groups")
+        #scanbool=office_inst.loadgroups(printLogs=True)
+        (groupBool)=office_inst.groupMaintenance(theWing,cwuA,itJSON,cwuA.activerun,printLogs=True)
+
+    else:
+        logging.info( "--------------------------------------------------------------------")
+        logging.info(" - Diagnostic Checks on Office 365 organizations [FINISHED]")
+        scanReturn=office_inst.scanCAPIDs(printLogs=True)
+        logging.info(" - All accounts have an account issued with Name, RANK and CAPID - [TESTING]")
+        (acctReturn,itJSONret)=office_inst.scanUNITs(theWing,cwuA,itJSON,cwuA.activerun,printLogs=True)
+        logging.info(" - [NOT FINISHED] - All accounts have a backup email (otherMail) assigned ")
+        logging.info(" - [NOT FINISHED] - Check Squadron CC all have aliases assigned ")
+        # History for each CAPID can be references as itJSON["NNNNNN"] where N is the local capid
+        wingJson.save_json_file(itJSONret,"it."+cwuA.todays_date+".json",print_logs=True)
+        logging.info("-----------------------------------")
+
+
     sys.exit(0)
 
 if __name__ == '__main__':
     main()
-
- 
--- capwatchutilities/cwuArguments.py
+++ capwatchutilities/cwuArguments.py
@@ -3,28 +3,64 @@ import time
 import getopt
 import logging
 import sys
+import os.path
+import string
+import random
 
 class cwuArguments():
     def __init__(self, fullCmdArguments):
-        self.today = datetime.date.today()
-        self.dateformed = self.today.strftime("%m")+"_"+self.today.strftime("%d")+"_"+self.today.strftime("%Y")
-        self.unixOptions = "ho:d:c:w:t:r:o:p:g:"
-        self.db_basename = datetime.datetime.now().strftime("%m_%d_%Y/%H_%M_%S_%f")
-        self.todays_date = datetime.datetime.now().strftime("%m_%d_%Y")
-        self.todays_time = datetime.datetime.now().strftime("%H_%M_%S_%f")
-        self.activerun = True
-        self.sendemail = True
-        self.test_this = True
-        self.capwatch_date=self.todays_date
-        self.gnuOptions = [
+        logging.debug( "This python provides a basic python library ")
+        logging.debug( " to help manage capwatch data for downstram ")
+        logging.debug( " use.                                       ")
+        logging.debug( "--------------------------------------------------------------------")
+        logging.debug(" FEATURE LIST ")
+        logging.debug(" Done?| Description                         ")
+        logging.debug( "--------------------------------------------------------------------")
+        logging.debug(" [YES]    | CAPWATCH DATABASE LOAD ")
+        self.today               = datetime.date.today()
+        self.dateformed          = self.today.strftime("%m")+"_"+self.today.strftime("%d")+"_"+self.today.strftime("%Y")
+        self.db_basename         = datetime.datetime.now().strftime("%m_%d_%Y/%H_%M_%S_%f")
+        self.todays_date         = datetime.datetime.now().strftime("%m_%d_%Y")
+        self.todays_time         = datetime.datetime.now().strftime("%H_%M_%S_%f")
+        self.activerun           = False
+        self.sendemail           = True
+        self.process_groups      = False
+        self.process_requests    = False
+        self.test_this           = True
+        self.path                = ""
+        self.password            = "myPassw0rd!"
+        self.capnhqurl           = "api/get.me"
+        self.baseurl             = "www.myurl.com"
+        self.regionid            = "REG"
+        self.topid               = 1
+        self.wingid              = "ST"
+        self.orgid               = 0
+        self.capwatch_date       = self.todays_date
+        self.capid               = 0
+        self.basedir             = "."
+        #    +cwuA.todays_date+".zip"
+        unixOptions         = "ho:d:c:w:t:r:o:p:g:"
+        gnuOptions = [
                            "help",
+                           "path=",
+                           "baseurl=",
+                           "capnhqurl=",
+                           "topid=",
+                           "regionid=",
+                           "wingid=",
+                           "capid=",
+                           "groups",
+                           "procreqs",
+                           "password=",
                            "activerun",
+                           "wingemailsraw",
+                           "orgid=",
                            "testit"
                            ]
         # GET AND PARSE ARGUMENTS
         argumentList = fullCmdArguments[1:]
         try:
-            arguments, values = getopt.getopt(argumentList, self.unixOptions, self.gnuOptions)
+            arguments, values = getopt.getopt(argumentList, unixOptions, gnuOptions)
         except getopt.error as err:
             # output error, and return with an error code
             print (str(err))
@@ -34,16 +70,42 @@ class cwuArguments():
                 sys.exit(0)
         else:
             for currentArgument, currentValue in arguments:
+                if(currentValue):
+                    logging.info(" Setting "+currentArgument+" to "+currentValue)
+                else:
+                    logging.info(" Setting "+currentArgument+" to True")
                 if currentArgument in ("-d", "--cwdate"):
                     self.capwatch_date=currentValue
                 elif currentArgument in ("--activerun"):
                     self.activerun = True
+                elif currentArgument in ("--procreqs"):
+                    self.process_requests = True
+                elif currentArgument in ("--groups"):
+                    self.process_groups = True
+                elif currentArgument in ("--topid"):
+                    self.topid = currentValue
+                elif currentArgument in ("--capid"):
+                    self.capid = currentValue
+                elif currentArgument in ("--password"):
+                    self.password = currentValue
+                elif currentArgument in ("--capnhqurl"):
+                    self.capnhqurl = currentValue
+                elif currentArgument in ("--wingid"):
+                    self.wingid = currentValue
+                elif currentArgument in ("--regionid"):
+                    self.regionid = currentValue
+                elif currentArgument in ("--baseurl"):
+                    self.baseurl = currentValue
                 elif currentArgument in ("--sendemail"):
                     self.sendemail = True
+                elif currentArgument in ("--orgid"):
+                    self.orgid = currentValue
                 elif currentArgument in ("--testit"):
                     self.test_this = True
                 elif currentArgument in ("-p", "--path"):
                     self.path=currentValue
+                    self.zipdir = os.path.dirname(self.path+os.sep+self.basedir+os.sep+self.todays_date+os.sep)
+                    logging.info(" Setting zipdir to "+self.zipdir+" [due to path setting]")
                 elif currentArgument in ("-h", "--help"):
                     self.showHelp()
                     sys.exit(0)
@@ -57,8 +119,18 @@ class cwuArguments():
         logging.info(" Current DATE:        | "+self.dateformed)
         logging.info(" Option | argument    | Description                  | Current Value")
         logging.info("  -h    | --help      | Print this help              | ")
-        logging.info("        | --cwdate    | Override CAPWATCH date       |["+self.capwatch_date+"] ")
         logging.info("        | --activerun | Actively run this command    |["+str(self.activerun)+"] ")
+        logging.info("        | --cwdate    | Override CAPWATCH date       |["+self.capwatch_date+"] ")
+        logging.info("        | --capnhqurl | URL for full capwatch down   |["+str(self.capnhqurl)+"] ")
+        logging.info("        | --procreqs  | Process Email Reqs Spreadsht |["+str(self.process_requests)+"] ")
+        logging.info("        | --groups    | Process Group Memberships    |["+str(self.process_groups)+"] ")
+        logging.info("        | --baseurl   | URL for full capwatch down   |["+str(self.baseurl)+"] ")
+        logging.info("        | --orgid     | ID for organization wanted   |["+str(self.orgid)+"] ")
+        logging.info("        | --regionid  | REGION ABBREVIATION          |["+str(self.regionid)+"] ")
+        logging.info("        | --topid     | ORG top level ID             |["+str(self.topid)+"] ")
+        logging.info("        | --capid     | CAP ID                       |["+str(self.capid)+"] ")
+        logging.info("        | --wingid    | WING ABBREVIATION            |["+str(self.wingid)+"] ")
+        logging.info("        | --password  | Account password             |["+str(self.password)+"] ")
         logging.info("--------------------------------------------------------------------")
     
     def get_random_password(self):
